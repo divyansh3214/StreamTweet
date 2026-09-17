a8de30cde9bbdd1bd5ce49cac55c84d960ca259b
divyansh3214 <divyanshjmi@gmail.com>
2026-09-17 19:24:59 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config({path:'./env'});
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js"
-import connectDB from "./db";
+import connectDB from "./db/index.js";
 
 /*
 import express from "express";
