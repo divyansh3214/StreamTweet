e1d18e7a20e2742bf3709e88e9e85f556d408ad4
divyansh3214 <divyanshjmi@gmail.com>
2026-09-20 06:43:39 +0000

    jwt tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,7 +55,31 @@ userSchema.pre("save", async function(next){
 })
 userSchema.methods.isPasswordCorrect=async function(password){
  return await bcrypt.compare(password,this.password);
-
 }
+userSchema.methods.generateAccessToken=function(){
+   return jwt.sign({
+      _id:this._id,
+      username:this.username,
+      email:this.email,
+      fullname:this.username,
+      fullname:this.fullname
+   },
+   process.env.ACCESS_TOKEN_SECRET,
+   {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRATION
+   }
+)
+}
+userSchema.methods.generateRefreshToken=function(){
+   return jwt.sign({
+      _id:this._id
+   },
+   process.env.REFRESH_TOKEN_SECRET,
+   {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRATION
+   }
+)
+}
+
 const User=mongoose.model('User', userSchema);
 export default User;
