a00cfe19716f9e0e2b1c26408bff38ce03bcd8df
divyansh3214 <divyanshjmi@gmail.com>
2026-09-21 10:41:44 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -10,4 +10,8 @@ app.use(express.json({limit:"1mb"}));
 app.use(express.urlencoded({extended:true,limit:"1mb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRoutes from "./routes/user.routs.js";
+app.use("/api/v1/users",userRoutes);
+
 export default app;
